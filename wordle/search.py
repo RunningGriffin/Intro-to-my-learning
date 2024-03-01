7ff6b86b69db2d91bdeb2d28a3faa4b8247b8e16
RunningGriffin <runninggriffin@gmail.com>
2024-03-01 16:52:27 -0700

    progress
    need to add subroutines to handle modification of possible word list/ threading has possible use case.

--- wordle/search.py
+++ wordle/search.py
@@ -7,48 +7,50 @@ def load_words(file_path):
 
 def main():
     # Replace 'wordlist.txt' with the path to your word list file
-    word_list = load_words('words.txt')
+    word_list = load_words('wordle\words.txt')
 
     # Initialize variables
     max_attempts = 6
     previous_guesses = set()
     possible_list = []
 
+    correct_letters = [None, None, None, None, None]
+
     for attempt in range(max_attempts):
         guess = input('Make a Guess ')
         print(f"Attempt {attempt + 1}")
         done = False
-        correct_positions = [None]
+        correct_positions = list()
+
         while not done:
         # Get user feedback (correct letters in correct positions)
             correct_position_index = int(input("Correct position index: (6 for done) "))
             if correct_position_index == 6:
                 done = True
             else:
+                correct_letters[correct_position_index] = guess[correct_position_index]
                 correct_positions.append(correct_position_index)
 
-        done = False
-        invalid_words = ['6']
-        while not done:
-        # Get user feedback (correct letters in correct positions)
-            invalid_word = input("invalid letters? (6 for done) ")
-            if invalid_word == '6':
-                done = True
-            else:
-                invalid_words.append(invalid_word)
+        print(correct_letters)
+        print(correct_positions)
 
-        sentinal = False
-        for word in word_list:
-            for index in correct_positions:
-                for invalid in invalid_words:
-                    if invalid in word:
-                        sentinal = True
-            if word[index] == guess[index] and sentinal == False:
-                possible_list.append(word)
-        
+
+        counter = 0
+        for index in correct_positions:
+            counter += 1
+            if counter == 1:
+                for word in word_list:
+                    if word[index] == guess[index]:
+                        possible_list.append(word)
+            else:
+                while index > len(possible_list):
+                    if word[index] != guess[index]:
+                        possible_list.remove(word)
+
+
         print(possible_list)
 
-    print("Out of attempts. Wordle not solved.")
+
 
 if __name__ == "__main__":
     main()
